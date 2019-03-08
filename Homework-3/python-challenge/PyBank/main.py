9fe05b13e033b6fab58c5101ba97c6c6e6da4f1e
kimberlyjharrison <kimberly.j.harrison@gmail.com>
2019-03-08 13:57:47 -0800

    working pyBank solution, needs comments

--- Homework-3/python-challenge/PyBank/main.py
+++ Homework-3/python-challenge/PyBank/main.py
@@ -2,7 +2,7 @@
 # Homework 3, Python PyBank Script
 # Submitted by Kim Harrison
 
-#import 
+#import dependencies
 import os
 import csv
 
@@ -11,19 +11,19 @@ monthList = []
 profitList = []
 changeList = []
 
-
 # Read in csv file
 csvpath = os.path.join("..", "Resources", "budget_data.csv")
 
 with open(csvpath, newline='') as csvfile:
 
-
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
 
+    #initialize variables for delta calculations
     maxGrowth = 0
     maxLoss = 0 
 
+    #read csv file into lists
     for row in csvreader:
         monthList.append(row[0])
         profitList.append(float(row[1]))
@@ -37,13 +37,13 @@ with open(csvpath, newline='') as csvfile:
         if deltaProfit < maxLoss:
             maxLoss = deltaProfit
             maxLossMonth = monthList[x+1]
-            
-        
+    
+    #Perform analysis
     numMonths = len(monthList)
     netProfit = sum(profitList)
     avgChange = (sum(changeList) / (numMonths - 1))
 
-
+#Print results to terminal
 print("---------------------------------------------------------------------------")
 print("Financial Analysis\n")
 print(f"Total Months: {numMonths}")
@@ -53,17 +53,15 @@ print(f"Greatest Month-over-Month Profit Increase: $({maxGrowth:0.2f}) in {maxGr
 print(f"Greatest Month-over-Month Profit Decrease: $({maxLoss:0.2f}) in {maxLossMonth}")
 print("---------------------------------------------------------------------------")
 
+#create new .txt file with financial analysis results
+output_file = os.path.join("financialAnalysis.txt")
 
-# numbers = [23.23, 0.123334987, 1, 4.223, 9887.2]
-
-# for number in numbers:
-#     print(f'{number:9.4f}')
-
-
-# Financial Analysis
-# ----------------------------
-# Total Months: 86S
-# Total: $38382578
-# Average  Change: $-2315.12
-# Greatest Increase in Profits: Feb-2012 ($1926159)
-# Greatest Decrease in Profits: Sep-2013 ($-2196167)
+with open("financialAnalysis.txt", "w") as text_file:
+    text_file.write("---------------------------------------------------------------------------\n")
+    text_file.write("Financial Analysis\n")
+    text_file.write(f"Total Months: {numMonths}\n")
+    text_file.write(f"Net Profits: $ {netProfit:0.2f}\n")
+    text_file.write(f"Average Change Month-over-Month: $({avgChange:0.2f})\n")
+    text_file.write(f"Greatest Month-over-Month Profit Increase: $({maxGrowth:0.2f}) in {maxGrowthMonth}\n")
+    text_file.write(f"Greatest Month-over-Month Profit Decrease: $({maxLoss:0.2f}) in {maxLossMonth}\n")
+    text_file.write("---------------------------------------------------------------------------")
