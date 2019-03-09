a1668580f5ffb9bf9df2f614be4dd6d5005b4a5c
kimberlyjharrison <kimberly.j.harrison@gmail.com>
2019-03-08 20:31:42 -0800

    Comments added to pyBank and pyPoll scripts

--- Homework-3/python-challenge/PyBank/main.py
+++ Homework-3/python-challenge/PyBank/main.py
@@ -1,6 +1,7 @@
 # UCIRV201902DATA3
 # Homework 3, Python PyBank Script
 # Submitted by Kim Harrison
+# Objective: use lists to read in a CSV file and analysis financial results
 
 #import dependencies
 import os
@@ -25,13 +26,16 @@ with open(csvpath, newline='') as csvfile:
 
     #read csv file into lists
     for row in csvreader:
-        monthList.append(row[0])
-        profitList.append(float(row[1]))
+        monthList.append(row[0]) #create list of month values
+        profitList.append(float(row[1])) #create list of profit values
         
-    for x in (range(len(profitList)-1)):
-        deltaProfit = profitList[x+1] - profitList[x]
-        changeList.append(deltaProfit)
-        if deltaProfit > maxGrowth:
+    #loop through data to find changes from one line to the next
+    for x in (range(len(profitList)-1)): #need to subtract 1 from the length of the data values for comparisons
+        deltaProfit = profitList[x+1] - profitList[x] #cacoluate change in profit over months
+        changeList.append(deltaProfit) #add change to new list
+        
+        #conditional to find max growth and max loss over the data. store values to counter variables.
+        if deltaProfit > maxGrowth: 
             maxGrowth = deltaProfit
             maxGrowthMonth = monthList[x+1]
         if deltaProfit < maxLoss:
@@ -39,9 +43,9 @@ with open(csvpath, newline='') as csvfile:
             maxLossMonth = monthList[x+1]
     
     #Perform analysis
-    numMonths = len(monthList)
-    netProfit = sum(profitList)
-    avgChange = (sum(changeList) / (numMonths - 1))
+    numMonths = len(monthList) #total months is lenght of list
+    netProfit = sum(profitList) #total profit is the sum of all profits
+    avgChange = (sum(changeList) / (numMonths - 1)) #average change is the sum of chang lists over the months minus 1
 
 #Print results to terminal
 print("---------------------------------------------------------------------------")
--- Homework-3/python-challenge/PyPoll/main.py
+++ Homework-3/python-challenge/PyPoll/main.py
@@ -2,67 +2,62 @@
 #Homework 3, Python PyPoll Script
 #Submitted by Kim Harrison
 #Objective: Using provided election data CSV, detemrine outcome of election
-#Skills: python Lits, Dictionaries
-
-# import os
-# import csv
-
-# csvpath = os.path.join("..", "Resources", "election_data.csv")
-
-# with open(csvpath, newline='') as csvfile:
-#     csvreader = csv.reader(csvfile, delimiter=',')
-#     csv_header = next(csvreader)
-
-#     voterDict={}
-#     for row in csvreader:
-#         voterDict[row[0]] = row[1:]
-
-# totalVotes = len(voterDict)
-# print(f"Total Votes: {totalVotes}")
-
-# print(voterDict)
+#Skills: Dictionaries
 
 import os
 import csv
-from collections import OrderedDict
 
+#read csv file
 csvpath = os.path.join("..", "Resources", "election_data.csv")
 
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
 
+#initialize new nested dictionary to track data
+#Key = Voter ID, value = County, Candidate
     voterDict={}
     for row in csvreader:
         voterDict[row[0]] = {}
         voterDict[row[0]]['county'] = row[1]
         voterDict[row[0]]['candidate'] = row[2]
 
+#determine total number of votes.  yikes thats a lot of votes!
 totalVotes = len(voterDict)
 
+#initialize list to track unique Candidates that received votes
 candidateList = []
+
+#add all candidates to a candidate list using a for-loop
 for x in voterDict:
     candidateList.append(voterDict[x]['candidate'])
-    
+
+#create new list of UNIQUE candidates using the 'set' method   
 candidates = set(candidateList)
+
+#initialize dictionary to keep track of candidates (key) and votes received (value)
 voteDict={}
 
-for candidate in candidates:
-    voteDict[candidate] = {}
-    counter = 0
-    for row in voterDict:
-        if voterDict[row]['candidate'] == candidate:
+#loop through the data to count candidate votes
+for candidate in candidates: #loop through each candidate's name
+    voteDict[candidate] = {} #create dictionary to keep track of each candidates votes
+    counter = 0 #inialize counter
+    for row in voterDict: #loop through all data to count votes
+        if voterDict[row]['candidate'] == candidate: #if the candidate is voted for, count the vote
             counter = counter + 1
-            voteDict[candidate]['votes'] = counter
-            voteDict[candidate]['percentage'] = counter/totalVotes * 100  
+            voteDict[candidate]['votes'] = counter #add vote count to dictionary
+            voteDict[candidate]['percentage'] = counter/totalVotes * 100  #calculate percentage of total votes
 
+#this part took me a long time to figure out, and right around here is where i realized that I
+#easily could have done this all using the candidateList created  in line 29.  Oh well, I need practice with dicts!
+#Call the sorted method to organize the dictionary in descending values by vote count
 sorted_d = sorted(voteDict.items(), key=lambda item: item[1]['votes'], reverse=True)
-print(sorted_d)
 
+#print results to terminal
 print("-------------------------------------------------------\n")
 print("Election Results\n")
 print("-------------------------------------------------------\n")
-for name in range(len(sorted_d)):
+for name in range(len(sorted_d)): #note: sorted_d is a list, not a dict!
     print(f"{sorted_d[name][0]}: {sorted_d[name][1]['percentage']:0.2f}% ({sorted_d[name][1]['votes']})")
 print("-------------------------------------------------------\n")
 print(f"Winner: {sorted_d[0][0]}\n")
