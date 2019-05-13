99d0d9791f8d652f4bbe32ccb23190ce92128e4f
kimberlyjharrison <kimberly.j.harrison@gmail.com>
2019-05-13 15:06:14 -0700

    added comments to flask app

--- Homework-10/app.py
+++ Homework-10/app.py
@@ -7,21 +7,26 @@ app = Flask(__name__)
 # Use flask_pymongo to set up mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
+#create index page
 @app.route("/")
 def index():
+    #pull data from mongodb
     mars_data = mongo.db.collection.find_one()
+    
+    #render html template
     return render_template("index.html", mars=mars_data)
 
-
+#create scrape route
 @app.route("/scrape")
 def scraper():
-    #mars = mongo.db.mars
+    #call scraping function
     mars_data = scrape_mars.scrape()
 
+    #add to mongodb
     mongo.db.collection.update({}, mars_data, upsert=True)
 
     return redirect("/", code=302)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
