0a083a7c8309541bb8ec4b3bb003d12474dfa504
kimberlyjharrison <kimberly.j.harrison@gmail.com>
2019-05-23 08:27:24 -0700

    completed level 1

--- Homework-11/static/js/app.js
+++ Homework-11/static/js/app.js
@@ -1,4 +1,31 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// assign variable to filter table button
+var submit = d3.select("#filter-btn");
+
+// assign variable to table body element
+var tbody = d3.select('tbody');
+
+// create event handler for button click to filter data
+submit.on('click', function() {
+
+	d3.event.preventDefault();
+
+	// select input element/test from input field
+	var inputElement = d3.select('#datetime');
+	var inputValue = inputElement.property('value');
+
+	//filter data based on input value
+	var filteredData = tableData.filter(inputDate => inputDate.datetime === inputValue);
+
+	//append table with data that matches filter criteria
+	filteredData.forEach((tableFilter) => {
+		var row = tbody.append('tr');
+		Object.entries(tableFilter).forEach(([key, value]) => {
+			var cell = row.append('td');
+			cell.text(value);
+		});
+	});
+});
+
